afd815cb433cbb55f51add5342d71b4ccbbed475
Tomonori <tomonori0204.takahashi@gmail.com>
2022-10-22 08:46:30 +0900

    Initial Commit

---
+++ MarubatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarubatsuApp
+//
+//  Created by 高橋知憲 on 2022/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
